4053305fbc729674af7bcc10dc8ff5619e92e54c
hridayroy19 <hrhridoyroy503@gmail.com>
2025-03-20 20:51:54 +0600

    user register and login add database

--- src/app.ts
+++ src/app.ts
@@ -2,16 +2,10 @@
 import express, { Application, NextFunction, Request, Response } from 'express';
 const app: Application = express();
 import cors from 'cors';
-import { BookRouter } from './app/modules/book/book.router';
-import orderRouter from './app/modules/order/order.router';
-import authRouter from './app/modules/auth/auth.router';
 import { StatusCodes } from 'http-status-codes';
-import cartRouter from './app/modules/addCartd/cart.route';
-// import paymentrouter from './app/modules/payments/payment.routes';
 import userRouter from './app/modules/user/user.routes';
-import paymentRouter from './app/modules/payments/paymentRouter';
-// import paymentRouter from './app/modules/payments/paymentRouter';
-// import paymentrouter from './app/modules/payments/payment.routes';
+import authRouter from './app/modules/auth/auth.router';
+import { ProjcetRouter } from './app/modules/book/projcet.router';
 
 //perser
 app.use(express.json());
@@ -19,13 +13,8 @@ app.use(cors({ credentials: true }));
 
 app.use('/api/auth', authRouter);
 app.use('/api/user', userRouter);
-//book router
-app.use('/api/products', BookRouter);
-//order router
-app.use('/api/cart', cartRouter);
-app.use('/api/order', orderRouter);
-app.use('/api', paymentRouter);
-// app.use('/api', paymentRouter);
+app.use('/api/projcet', ProjcetRouter);
+
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Server site is running');
--- src/app/modules/addCartd/cartInterface.ts
+++
@@ -1,9 +0,0 @@
-export type TCart = {
-  productId: string;
-  title: string;
-  price: number;
-  image: string;
-  userEmail: number;
-  createdAt?: Date;
-  updatedAt?: Date;
-};
